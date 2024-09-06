b0b40d21510b4a90f0ff64fe35783c03ad9a8dd0
Antônio Antunes <antunesantonio91@gmail.com>
2024-09-06 10:21:29 -0300

    começando o blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    categoria = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=600)
+    subtitulo = models.CharField(max_length=100)
+    conteudo = models.TextField(max_length=1200)
+    imagem = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.titulo
